74d64f835bbef9b5c615cbda6fccb80423344929
MargoMoran <margomoranstarzhy@gmail.com>
2020-08-14 19:51:53 +0300

    Add task #4 and comments

--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -5,7 +5,11 @@ public class Tasks {
         whatSeason();
         whatPrise();
         fingerNumber();
+        whichDay();
     }
+
+    //Написать метод который принимает в себя текст сезон (“summer”, “winter”...) - переменную типа String,
+    // и если сезон - winter выводит в консоль “It is cold”.
     static void whatSeason(){
         Scanner s = new Scanner(System.in);
         System.out.println("What season is it: Winter, Spring, Summer or Autumn?");
@@ -28,6 +32,9 @@ public class Tasks {
                         break;
                 }   System.out.println();       }
 
+//Написать метод который принимает в себя цену - переменную типа int,
+// и если цена меньше 1000 (prise < 1000), то выводит в консоль “цена нормальная”,
+//если больше или равно 1000 (prise >= 1000), то выводит в консоль “цена завышена”
     static void whatPrise(){
         Scanner s = new Scanner(System.in);
         System.out.println("Какая цена?");
@@ -35,6 +42,9 @@ public class Tasks {
         if (prise<1000) System.out.println("Цена нормальная\n");
         else if (prise>=1000) System.out.println("Цена завышена\n"); }
 
+
+//Написать метод, используя switch, который принимает в себя номер пальца -
+// переменную типа int и в зависимости от номер выводит название пальца (1-”большой” и т д)
     static void fingerNumber() {
         Scanner s = new Scanner(System.in);
         System.out.println("Какой палец?");
@@ -58,13 +68,18 @@ public class Tasks {
             default:
                 System.out.println("Ты - мутант!");
                 break;
-        }}
+        }   System.out.println();       }
 
-
-
-
-
-
+        //Написать метод который принимает на вход String - день недели, и если это будний день, то выводит “будний”,
+        // если выходной - “выходной” (используя иф-елс и логические выражения)
+    static void whichDay() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Выбери какой день: Пн, Вт, Ср, Чт, Пт, Сб, Вс?");
+        String today = s.nextLine();
+        if (today.contains("Сб") || today.contains("Вс")){
+        System.out.println("Выходной!");        }
+        else if (today.contains("Пн") || today.contains("Вт") || today.contains("Ср") || today.contains("Чт") || today.contains("Пт"))System.out.println("Будний=(");
+        else System.out.println("Ты неправильно ввел день недели");}
         }
 
 
